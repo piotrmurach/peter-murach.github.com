a9a1f79568fcce739b719b1af07b8ba295829927
Piotr Murach <pmurach@gmail.com>
2011-12-27 18:52:00 +0000

    Further extend loaded javascripts.

--- js/application.js
+++ js/application.js
@@ -1,3 +1,11 @@
-require(["libs/jquery-1.7.1.min", "libs/modernizr-2.0.6" ], function($) {
+require([
+  "libs/jquery-1.7.1.min",
+  "libs/modernizr-2.0.6",
+  "libs/plugins",
+  "libs/respond.min"
+  ], function($) {
   //callback to fire up any extra scripts
+  $(function(){
+    
+  });
 });
