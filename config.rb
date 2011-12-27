52ff4582c7a85a30c2ef139cd356f8695d525933
Piotr Murach <pmurach@gmail.com>
2011-12-27 18:53:42 +0000

    Add initial setup for stylesheets.

---
+++ config.rb
@@ -0,0 +1,15 @@
+require 'compass-h5bp'
+require 'html5-boilerplate'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions.
+# relative_assets = true
