6a23108ea343d5761fec8e1a57fe1dbfb24157e7
Piotr Murach <pmurach@gmail.com>
2012-01-02 20:44:07 +0000

    Add png transparent for ie.

--- js/application.js
+++ js/application.js
@@ -2,9 +2,11 @@ require([
   "libs/jquery-1.7.1.min",
   "libs/modernizr-2.0.6",
   "libs/plugins",
-  "libs/respond.min"
+  "libs/respond.min",
+  "libs/jquery.pngFix.pack"
   ], function() {
   //callback to fire up any extra scripts
-  $(function(){
-  });
+    $(document).ready(function() {
+      $(document).pngFix();
+    });
 });
